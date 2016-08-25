48f6ddb751efe11f506d817f54ea6652074fea67
Weyand Tim <tim.weyand@R1717.local>
2016-08-25 12:09:47 +0200

    fixed error in bash script

--- scripts/klambt_docker_varnish_configure.sh
+++ scripts/klambt_docker_varnish_configure.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ ! -z $DRUPAL_VARNISH_KEY]; then;
+if [ ! -z $DRUPAL_VARNISH_KEY ]; then
     echo $DRUPAL_VARNISH_KEY > /etc/varnish/secret
 else
-    if [ ! -z $VARNISH_KEY]; then;
+    if [ ! -z $VARNISH_KEY ]; then
         echo $VARNISH_KEY > /etc/varnish/secret
     else
         uuidgen > /etc/varnish/secret
